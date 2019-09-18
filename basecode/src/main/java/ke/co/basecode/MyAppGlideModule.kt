9141b78361fd82d21309febd18c81536702d963a
Think Synergy <thinksynergy@thinksynergy.co.ke>
2019-09-19 01:16:04 +0300

    Added glide module

---
+++ basecode/src/main/java/ke/co/basecode/MyAppGlideModule.kt
@@ -0,0 +1,15 @@
+package ke.co.basecode
+
+/**
+ * Created by William Makau on 13/07/2019.
+ * Company: Think Synergy Limited
+ * Email: williammakau070@gmail.com
+ * Phone: +254706356815
+ * Purpose:
+ */
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+//Defines a set of dependencies and options to use when initializing Glide within an application
+@GlideModule
+class MyAppGlideModule : AppGlideModule()

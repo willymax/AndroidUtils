bdc1ceaa679cc0e127d7d7ab8037a5df962ca903
william makau <williammakau070@gmail.com>
2020-03-24 14:19:22 +0300

    Changed ApiError object

--- basecode/src/main/java/ke/co/basecode/model/APIError.kt
+++ basecode/src/main/java/ke/co/basecode/model/APIError.kt
@@ -3,7 +3,11 @@ package ke.co.basecode.model
 
 data class APIError<T> (
     val status_code : Int? = -1,
+    val meta : ErrorMeta?,
     var message: String? = "Request failed",
-    var success: Boolean? = false,
-    var data: T? = null
+    var success: Boolean? = false
+)
+data class ErrorMeta(
+    val code : String,
+    val message: String
 )
